987af926675c798d51bc989c5e48393f220c87ca
GhostButter <wyattjones04@me.com>
2015-12-02 15:38:10 -1000

    sound code
    rhgrtw

---
+++ sup.js
@@ -0,0 +1,16 @@
+var Uscore = 0;
+
+var highscore = 9001;
+
+var audio = new Audio('mario.wav');
+
+var press = function() {
+	Uscore=Uscore+9999;
+	audio.play();
+	document.getElementById('score').innerHTML = "Your Score " +Uscore;
+	if (Uscore > highscore) {
+	document.getElementById('hiscore').innerHTML = "New Highscore" +Uscore;
+	return;
+
+	}
+};
