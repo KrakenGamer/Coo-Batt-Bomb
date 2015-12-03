c8399e853ef9eff3a015f99edfd4d514f47d5855
coolernow <coolernow641@gmail.com>
2015-12-02 16:56:43 -1000

    MOAR PROGAMMIN

--- sup.js
+++ sup.js
@@ -1,16 +1,10 @@
-var Uscore = 0;
 
-var highscore = 9001;
+var mk7 = new Audio('mk7_home_menu_icon.wav');
+var something = new Audio('sm64_mario_doh.wav')
 
-var audio = new Audio('mk7_home_menu_icon.wav');
-
-var press = function() {
-	Uscore=Uscore+9999;
-	audio.play();
-	document.getElementById('score').innerHTML = "Your Score " +Uscore;
-	if (Uscore > highscore) {
-	document.getElementById('hiscore').innerHTML = "New Highscore" +Uscore;
-	return;
-
-	}
+var pressmk7 = function() {
+	mk7.play();
+};
+var presssomething = function(){
+	something.play();
 };
