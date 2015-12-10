7584f76330ee7bf028e757ecc2329202d8b3b75c
coolernow <coolernow641@gmail.com>
2015-12-09 16:18:02 -1000

    Fin.
    YO FACE

--- sup.js
+++ sup.js
@@ -1,10 +1,14 @@
 
 var mk7 = new Audio('mk7_home_menu_icon.wav');
 var something = new Audio('sm64_mario_doh.wav')
+var poot = new Audio('heavy_needdispenser01.mp3')
 
 var pressmk7 = function() {
 	mk7.play();
 };
 var presssomething = function(){
 	something.play();
+};
+var presspoot = function(){
+	poot.play();
 };
